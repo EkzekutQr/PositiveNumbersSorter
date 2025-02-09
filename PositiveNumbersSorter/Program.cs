ec385bb8b57b6ada7f60165175240819e2345ab1
EkzekutQr <ekzekutqr@gmail.com>
2025-02-10 02:33:28 +0500

    Initial commit

---
+++ PositiveNumbersSorter/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+// Пример массива целых чисел
+int[] numbers = { -5, 10, -3, 8, -1, 15, 0, 7, -2, 12 };
+
+// Фильтруем положительные элементы
+var positiveNumbers = numbers.Where(n => n > 0).ToArray();
+
+// Выводим положительные элементы в обратном порядке
+Console.WriteLine("Положительные элементы в обратном порядке:");
+for (int i = positiveNumbers.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(positiveNumbers[i]);
+}
